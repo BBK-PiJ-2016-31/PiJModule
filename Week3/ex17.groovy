7685e1617d2e5685d3fe82f804b06f1b77659ae6
Damanjit <me@damanjit.com>
2016-10-17 18:42:01 +0100

    Warning messages corrected. Separate function for readMessage

--- Week3/ex17.groovy
+++ Week3/ex17.groovy
@@ -1,4 +1,3 @@
-
 println " Welcome to My Mail Server!"
 int step=1
 boolean valid = false
@@ -16,26 +15,12 @@ while (step<4) {
 		valid = checkInput("RCPT TO: ", input, step)
 	} else if (step==3) {
 		if (input=="DATA"){
-			String user = System.console().readLine()
-			String message = ""
-			while (user!="."){
-				message = message + user + '\n' 
-				user = System.console().readLine()				
-			}
-			println "Sending email..."
-			println "from: " + from
-			println "to: " + to
-			println message
-			println "...done!"
+			readSendEmailMessage()
 			step=0			// So we don;t print OK in the last step
 			valid = true	// so we can reset step back to 1 below
 		}
 	}
-
-	if (!valid) { 
-		if (!warning) {println "Invalid command."}
-		warning = false
-		} else {
+	if (valid) { 
 			if (step!=0) {println 'OK'}
 			step += 1
 			valid = false
@@ -43,14 +28,31 @@ while (step<4) {
 }
 println " Bye! "
 
+void readSendEmailMessage() {
+	String user = System.console().readLine()
+	String message = ""
+	while (user!="."){
+		message = message + user + '\n' 
+		user = System.console().readLine()				
+	}
+	println "Sending email..."
+	println "from: " + from
+	println "to: " + to
+	print message
+	println "...done!"
+}
 
 boolean checkInput(String valid, String input, int step){
-	if (input.length()<valid.length()) { return false}
+	if (input.length()<valid.length()) { 
+		println "Invalid command."
+		return false
+	}
 
 	for (int i=0; i<valid.length();i++){	// Check the first part first i.e. ' MAIL FROM: '
 		char check1 = valid.charAt(i)
 		char check2 = input.charAt(i)
 		if (check1 != check2) {
+			println "Invalid command."
 			return false
 		} // first part matched then move forward else return	
 	}
@@ -60,8 +62,8 @@ boolean checkInput(String valid, String input, int step){
 
 	if (check1.isLetter()) { // if the next is letter than lets check for the ' @ ' and ' . '
 		String email = input.substring(valid.length(), input.length()) // capture the email part
-		if (step==1) { from = email}		
-		if (step==2) { to = email}
+		if (step==1) { from = email }		
+		if (step==2) { to = email }
 		for (int i=0; i<email.length(); i++){
 			char temp = email.charAt(i)
 			if (temp=='@'){
@@ -75,13 +77,11 @@ boolean checkInput(String valid, String input, int step){
 			return true
 		} else {
 			println " Invalid email address "
-			warning = true
 			return false
 		}
 
 	} else {
 		println " Invalid email address "
-		warning = true
 		return false
 	}
 }
