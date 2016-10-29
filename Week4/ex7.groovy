3385d13e26b6cd8ab330d08a37189223903804fb
Damanjit <me@damanjit.com>
2016-10-29 16:13:49 +0100

    Answer checked and corrected. code corrected.

--- Week4/ex7.groovy
+++ Week4/ex7.groovy
@@ -1,14 +1,34 @@
 print " Enter total amount borrowed: "
 String str = System.console().readLine()
 double totalBorrowed = Double.parseDouble(str)
+
 print " Enter total number of years to pay: "
 str = System.console().readLine()
 double yearsToPay = Double.parseDouble(str)
+
 print " Enter interest rate charged: "
 str = System.console().readLine()
 double interestRate = Double.parseDouble(str)
 
 double t = totalBorrowed * (1+ (interestRate/100))
+double yearlyPayment = (BigDecimal)t / yearsToPay
+double interestTotal = t - totalBorrowed
 
-println ("Total to be paid: " + t)
-println ("Every year " + t/yearsToPay+" has to paid")
+println "--------------------------------------------------"
+println "Total to be paid: " + t
+println "Every year " + yearlyPayment+" has to paid"
+println "Interest total : " + interestTotal
+
+years2Pay(interestTotal, yearlyPayment)
+
+void years2Pay (double interestTotal, double yearlyPayment){
+		int years = 0
+		while (interestTotal > yearlyPayment){
+			years++
+			interestTotal -= yearlyPayment
+		}
+		if (years==0) {
+			years=1
+		} 
+		println "Interest will finish within " + years + " year(s)"
+}
