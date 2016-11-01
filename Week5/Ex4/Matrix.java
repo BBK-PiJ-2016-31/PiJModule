d07a1bb80603e914d12fd1ec5b2220fe2c3b9bfc
Damanjit <me@damanjit.com>
2016-11-01 20:55:10 +0000

    "Method will do nothing" so removed the ring statement

--- Week5/Ex4/Matrix.java
+++ Week5/Ex4/Matrix.java
@@ -1,15 +1,35 @@
 public class Matrix{
 	int[][] array;
+	int row, column;
 	public Matrix (int row, int column){
+		this.row = row;			// to use at other places
+		this.column = column; // to use them in other functions
 		array = new int [row] [column];
 		for (int i=0 ; i<row ; i++){
 			for (int j=0 ; j<column ; j++){
 				array [i][j] =1;
-				System.out.print ("Array["+i+"] ["+j+"] = "+ array[i][j]);
+			}
+		}
+//		print();
+	}
+
+	public void setElement(int row, int column, int value){
+		if ((row<=this.row) && (column<=this.column)) {
+			array[row][column]=value;
+//			print();
+//		}else {
+//			System.out.println ("** Position out of bounds **");
+		}
+	}
+
+
+	public void print(){
+		for (int i=0 ; i<row ; i++){
+			for (int j=0 ; j<column ; j++){
+				System.out.print ("Array["+i+"]["+j+"]="+ array[i][j]+"  ");
 			}
 			System.out.println("");
 		}
-
 	}
 
 }
