eb884ed480f4726935196c7732865dad74e10949
Damanjit <me@damanjit.com>
2016-11-03 21:58:56 +0000

    Working on isTriangular()

--- Week5/Ex6/MatrixChecker.java
+++ Week5/Ex6/MatrixChecker.java
@@ -1,8 +1,9 @@
 public class MatrixChecker{
+	boolean symmetrical2 = false;
 
 	public boolean isSymmetrical(int[] arrayOfMatrix){
 		boolean isOrNot = true;	// assume it is symmetrical
-		for (int i=0; i<arrayOfMatrix.length/2; i++){
+		for (int i=0; i<arrayOfMatrix.length; i++){
 			if (arrayOfMatrix[i]!=arrayOfMatrix[arrayOfMatrix.length-i-1]){
 				isOrNot = false;
 			}
@@ -10,4 +11,31 @@ public class MatrixChecker{
 		return isOrNot;
 	}
 
+	public boolean isSymmetrical(int[][] arrayOfMatrix){
+		symmetrical2 = true;
+		return (isTriangular(arrayOfMatrix));
+	}
+
+	public boolean isTriangular(int[][] arrayOfMatrix){
+		int rowLength = arrayOfMatrix.length;			// row length
+		int columnLength = arrayOfMatrix[0].length; // column length. All column lengths will be the same
+		boolean isOrNot = true;	// assume result is correct
+		for (int i=0; i<rowLength; i++){
+			for (int j=0;j<columnLength;j++){
+				if (symmetrical2==true){
+					if (arrayOfMatrix[i][j]!=arrayOfMatrix[j][i]){
+						isOrNot = false;
+					}
+				}else {
+					if (i>j){
+				 		if (arrayOfMatrix[i][j]!=0){
+				 			isOrNot = false;
+					 	}
+					}
+				}			
+			}
+		}
+		symmetrical2 = false;
+		return isOrNot;
+	}
 }
--- Week5/Ex6/MatrixCheckerTester.groovy
+++ Week5/Ex6/MatrixCheckerTester.groovy
@@ -1,4 +1,8 @@
 MatrixChecker abc = new MatrixChecker();
 int[] check1 = [1,2,3,4,5,6,5,4,3,2,1];
-int[][]check2 = [1,2,3;4,5,6;7,8,9]
-println (abc.isSymmetrical(check));
+int[][] check2 = [[1,2,3],[2,4,5],[3,5,8]]
+int[][] check3 = [[1,2,3],[0,5,6],[0,0,9]]
+
+// println (abc.isSymmetrical(check1));
+//println (abc.isSymmetrical(check2));
+println (abc.isSymmetrical(check3));
