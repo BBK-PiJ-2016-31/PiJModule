c4bdac718af9fc4bc854dcd551f4f990dfaba835
Damanjit <me@damanjit.com>
2017-01-22 17:51:58 +0000

    Folders Updated. Ex3 updated

--- week12/ex2/HashTest.java
+++ week12/ex2/HashTest.java
@@ -6,9 +6,9 @@ public class HashTest{
     @Test
     public void testShortHash(){      
     	int hash;  
-        for(int i = 0; i < 1000; i++){
+        for(int i = 0; i < 5000; i++){
             hash = HashUtilities.shortHash(i);
-            assertTrue(String.format("%d returns a hash: %d that is out of range", i, hash), 0 <= hash && hash <= 998);
+            assertTrue(String.format("%d returns a hash: %d that is out of range", i, hash), 0 <= hash && hash <= 1000);
         }
     }
 }
