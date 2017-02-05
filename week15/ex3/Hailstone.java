9582920be541786d7e42b33573650b7eb7f9f52f
Damanjit <me@damanjit.com>
2017-02-05 20:06:54 +0000

    Code progressed.

--- week15/ex3/Hailstone.java
+++ week15/ex3/Hailstone.java
@@ -5,12 +5,11 @@ public class Hailstone{
 	private static ArrayList<Integer> result = new ArrayList<>();
 
 	public static ArrayList hail1(int n){
+		result.add(n);
 		if (n==1) return result;
 		if (n%2==0){
-			result.add(n);
 			hail1(n/2);
 		} else {
-			result.add(n);
 			hail1((3*n)+1);
 		}
 		return result;
--- week15/ex3/Main.java
+++ week15/ex3/Main.java
@@ -4,9 +4,9 @@ import java.time.Duration;
 public class Main{
 	public static void main (String[] args){
 		Instant start = Instant.now();
-		System.out.println (Hailstone.hail1(2));
+		System.out.println (Hailstone.hail1(200000));
 		Instant end = Instant.now();
 		Duration timeTaken = Duration.between(end,start);
-		System.out.println ("Time taken: "+ timeTaken.getNano());
+		System.out.println ("Time taken: "+ timeTaken.toSeconds());
 	}
 }
