401378b81c38fe75fe3a3c5b0901d5a040407766
Damanjit <me@damanjit.com>
2017-02-23 13:17:04 +0000

    New classes added

---
+++ DAY17_CONCUR/ex1/src/TextLoop.java
@@ -0,0 +1,20 @@
+/**
+ * Created by D on 20/02/2017.
+ */
+public class TextLoop implements Runnable {
+
+    public static final int COUNT = 10;
+
+    private final String name;
+
+    public TextLoop(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < COUNT; i++) {
+            System.out.println("Loop:" + name + ", iteration:" + i + ".");
+        }
+    }
+}
---
+++ cw-ds/feedback/README.md
@@ -0,0 +1,8 @@
+The code won't compile:
+
+Delete.java:2: error: class Main is public, should be declared in a file named Main.java
+public class Main {
+       ^
+1 error
+
+You should ensure that the code you submit does compile.
