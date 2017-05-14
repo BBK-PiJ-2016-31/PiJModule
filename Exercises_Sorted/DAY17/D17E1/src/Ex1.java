f774d2332f5337ab1815b3366237e498831222a6
Damanjit <me@damanjit.com>
2017-05-14 13:32:53 +0100

    Reorganisation of files

---
+++ Exercises_Sorted/DAY17/D17E1/src/Ex1.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Ex1 ex = new Ex1();
+		ex.launch();
+
+	}
+	public void launch(){
+			File file = new File ("/");
+			String list[] = file.list();
+			int counter=1;
+			for (String str: list){
+				System.out.println (counter+") "+str);
+				counter++;
+			}
+		}
+		
+	}
---
+++ Exercises_Sorted/DAY17/D17E8/src/Temperature.java
@@ -0,0 +1,6 @@
+/**
+ * Created by D on 14/05/2017.
+ */
+public class Temperature {
+
+}
